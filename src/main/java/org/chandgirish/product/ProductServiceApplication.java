1cdc865d424cab210eb5c43e5b5da2395e875776
Girish Chand <girishchand1095@gmail.com>
2026-03-29 11:59:58 +0100

    initial commit

---
+++ src/main/java/org/chandgirish/product/ProductServiceApplication.java
@@ -0,0 +1,18 @@
+package org.chandgirish.product;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+//        TWO WAYS FOR SECRET HANDLING
+//        Dotenv dotenv = Dotenv.load();
+//        System.out.println("THE PORT IS : " + dotenv.get("PORT"));
+//        System.setProperty("PORT", dotenv.get("PORT"));
+
+        Dotenv.configure().ignoreIfMissing().load(); // loads all .env variables
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
